77a60525785401bc115f2aaf32095c8f89df4a20
Isaiah Guardado <gisaiah416@gmail.com>
2021-12-18 20:04:38 -0800

    If product count is 0 then it will gray out add cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import { Container } from 'react-bootstrap';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
